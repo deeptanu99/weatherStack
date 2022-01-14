08b53c224591adfbb1f0bea6ec8a225bf072969b
deeptanu <deeptanubhowmik99@gmail.com>
2022-01-14 19:09:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+const api={
+  main:'https://api.openweathermap.org/data/2.5/weather?q=',
+  access_key:'f448473a2879b59a6f823a81374a6c75'
+}
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = event => {
+    if (event.key === "Enter") {
+      fetch(`${api.main}${query}&appid=${api.access_key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+ 
+  function getFormattedDate(today) 
+{
+    var week = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+    var day  = week[today.getDay()];
+    var dd   = today.getDate();
+    var mm   = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    var hour = today.getHours();
+    var minu = today.getMinutes();
+
+    if(dd<10)  { dd='0'+dd } 
+    if(mm<10)  { mm='0'+mm } 
+    if(minu<10){ minu='0'+minu } 
+
+    return day+' - '+dd+'/'+mm+'/'+yyyy+' '+hour+':'+minu;
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text" className="search-bar" placeholder="Enter the city name here"
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{getFormattedDate(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)-273}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
     </div>
   );
 }
